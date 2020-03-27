11ce64b4eb69c04e86ba24254f97744d1da4c59c
Wioletta Dziwińska <dziwinskawioletta@gmail.com>
2020-03-27 22:59:04 +0100

    inicial commit

---
+++ cmake-build-debug/CMakeFiles/Rekurencja_silnia.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rekurencja_silnia.dir/main.c.obj"
+  "Rekurencja_silnia.exe"
+  "Rekurencja_silnia.exe.manifest"
+  "Rekurencja_silnia.pdb"
+  "libRekurencja_silnia.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rekurencja_silnia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int silnia(int n){
+    int suma_n=0;
+
+    if(n==0) {
+        return 1;
+    }
+    else{
+        suma_n = n * silnia(n - 1);
+        return suma_n;
+    }
+}
+int main() {
+    int silnia_n=0;
+    silnia_n=silnia(4);
+    printf("%d", silnia_n);
+    return 0;
+}
